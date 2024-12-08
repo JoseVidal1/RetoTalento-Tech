dc3e3090002087725ac0a9c446b722a43d2c9791
Alvaro Jose Vidal Bello <retrosuun@gmail.com>
2024-12-07 19:36:54 -0500

    Creacion Home proyecto y carpetas

---
+++ Presentacion/script.js
@@ -0,0 +1,9 @@
+
+//Scrip para La hamburgesa = Responsive
+let bar = document.getElementById("bar")
+let nav = document.getElementById("nv")
+
+bar.onclick= () =>{
+    nav.classList.toggle("active")
+}
+
